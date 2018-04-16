9724dbbbba67e68041b88b77d61200d8f971ca0f
Julian Suringa <julian.suringa@avensia.com>
2018-04-16 15:56:48 +0800

    webpack 4 starter setup

---
+++ src/AppComponent.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class AppComponent extends Component {
+  render() {
+    return (
+        <div>
+          App Component is Rendered!!
+          webpack  4 starter.
+        </div>
+    )
+  }
+}
